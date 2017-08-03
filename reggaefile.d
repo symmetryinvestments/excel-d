cb8c71b666f7bd16019a9e98ebfcfbd2e8f3ad53
Atila Neves <aalvesne@cisco.com>
2017-08-03 22:07:12 +0200

    Updated reggaefile.d

--- reggaefile.d
+++ reggaefile.d
@@ -1,8 +1,7 @@
 import reggae;
 import std.typecons;
 
-alias ut = dubTestTarget!();
-alias utl = dubConfigurationTarget!(ExeName("utl"),
-                                    Configuration("ut"),
-                                    Flags("-unittest -version=unitThreadedLight"));
+alias ut = dubTestTarget!(Flags("-g -debug"));
+alias utl = dubConfigurationTarget!(Configuration("ut"),
+                                    Flags("-unittest -version=unitThreadedLight -g -debug"));
 mixin build!(ut, utl);
