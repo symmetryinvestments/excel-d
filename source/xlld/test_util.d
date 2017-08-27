6e751793501f1c96393c7cddeb54259605158617
9il <ilyayaroshenko@gmail.com>
2017-08-26 21:54:57 -0700

    remove xlld.static_ module, move unittest module constructors to xlld.test_util

--- source/xlld/test_util.d
+++ source/xlld/test_util.d
@@ -19,6 +19,23 @@ void*[maxCoerce] gFreed;
 double[] gDates;
 double[] gTimes;
 
+version(unittest)
+{
+    static this() {
+        import xlld.xlcallcpp: SetExcel12EntryPt;
+        import xlld.test_util: excel12UnitTest;
+        // this effectively "implements" the Excel12v function
+        // so that the code can be unit tested without needing to link
+        // with the Excel SDK
+        SetExcel12EntryPt(&excel12UnitTest);
+    }
+
+    static ~this() {
+        import xlld.test_util: gCoerced, gNumXlCoerce, gFreed, gNumXlFree;
+        import unit_threaded.should: shouldBeSameSetAs;
+        gCoerced[0 .. gNumXlCoerce].shouldBeSameSetAs(gFreed[0 .. gNumXlFree]);
+    }
+}
 
 extern(Windows) int excel12UnitTest(int xlfn, int numOpers, LPXLOPER12 *opers, LPXLOPER12 result) nothrow @nogc {
 
