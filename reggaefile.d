a22b744fa1b72714142151cb57ae72bc4b9867b0
Atila Neves <atila.neves@gmail.com>
2018-02-28 18:54:53 +0000

    Better reggaefile

--- reggaefile.d
+++ reggaefile.d
@@ -1,7 +1,12 @@
 import reggae;
 import std.typecons;
 
-alias ut = dubTestTarget!(CompilerFlags("-g -debug"));
+alias ut = dubTestTarget!(CompilerFlags("-g -debug"),
+                          LinkerFlags(),
+                          No.allTogether);
 alias utl = dubConfigurationTarget!(Configuration("ut"),
-                                    CompilerFlags("-unittest -version=unitThreadedLight -g -debug"));
+                                    CompilerFlags("-unittest -version=unitThreadedLight -g -debug"),
+                                    LinkerFlags(),
+                                    Yes.main,
+                                    No.allTogether);
 mixin build!(ut, utl.optional);
