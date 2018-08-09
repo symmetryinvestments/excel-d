494df087b79106c6e765e3d996b0e6fb8f9323b4
Atila Neves <atila.neves@gmail.com>
2018-08-09 17:45:14 +0200

    Only use DIP1000 with dmd

--- source/xlld/wrap/wrap.d
+++ source/xlld/wrap/wrap.d
@@ -382,26 +382,27 @@ private XLOPER12* callWrapped(alias wrappedFunc, T)(scope T dArgs)
 }
 
 
-@("@safe functions have to use scope on parameters with indirections")
-@safe unittest {
-    import std.typecons: tuple;
-    import std.conv: text;
+version(DIP1000) {
+    @("@safe functions have to use scope on parameters with indirections")
+        @safe unittest {
+        import std.typecons: tuple;
+        import std.conv: text;
 
-    // make sure we can't escape parameters
-    static int oops(int* i) @safe;
-    static int good(scope int* i) @safe;
-    int val = 42;
-    auto valPtr = () @trusted { return &val; }();
+        // make sure we can't escape parameters
+        static int oops(int* i) @safe;
+        static int good(scope int* i) @safe;
+        int val = 42;
+        auto valPtr = () @trusted { return &val; }();
 
-    // calling directly is ok
-    static assert(__traits(compiles, good(valPtr)));
-    static assert(__traits(compiles, oops(valPtr)));
+        // calling directly is ok
+        static assert(__traits(compiles, good(valPtr)));
+        static assert(__traits(compiles, oops(valPtr)));
 
-    // calling through callWrapped only ok if the param is `scope`
-    static assert( __traits(compiles, callWrapped!good(tuple(valPtr))));
-    static assert(!__traits(compiles, callWrapped!oops(tuple(valPtr))));
+        // calling through callWrapped only ok if the param is `scope`
+        static assert( __traits(compiles, callWrapped!good(tuple(valPtr))));
+        static assert(!__traits(compiles, callWrapped!oops(tuple(valPtr))));
+    }
 }
-
 
 /**
    Return an autofreeable XLOPER12 from a string to return to Excel.
